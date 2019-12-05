7bfc89ad8e6d47de555255ab7efee6dd79b377ec
Najibullah Karimi <nkarimi@SWDV.dom>
2019-12-04 18:55:45 -0700

    Project05

---
+++ WebApplication1/Maintenance/checkout.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" 
+    CodeBehind="checkout.aspx.cs" Inherits="WebApplication1.Maintenance.checkout" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+
+
+
+
+   
+
+       <h2> <p>
+        <br />
+        Disk Checkout
+        </p></h2>
+    <br />
+    <p>
+        Select a Disk:</p>
+    <p>
+    <%-- DropDown Disk, validator and connection string db --%>
+    <asp:DropDownList ID="ddlDisk" runat="server" DataSourceID="SqlDataSource1" 
+        DataTextField="disk_name" DataValueField="disk_id" OnDataBound="ddlDisk_DataBound" >
+    </asp:DropDownList>
+
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" CssClass="text-danger"
+            ErrorMessage=" *Disk Selection is Required" ControlToValidate="ddlDisk">
+        </asp:RequiredFieldValidator>
+
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+        ConnectionString="<%$ ConnectionStrings:disk_inventoryjkConnectionString2 %>" 
+            SelectCommand="SELECT [disk_id], [disk_name] FROM [disk] 
+                WHERE ([status_id] = @status_id) ORDER BY [disk_name]">
+        <SelectParameters>
+           <asp:Parameter DefaultValue="1" Name="status_id" Type="Int32" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+
+         </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        Select a Borrower:</p>
+    <p>
+         <%-- DropDown Borrower, validator and connection string db --%>
+        <asp:DropDownList ID="ddlBorrower" runat="server" DataSourceID="SqlDataSource2" 
+            DataTextField="lname" DataValueField="borrower_id" OnDataBound="ddlBorrower_DataBound" >
+        </asp:DropDownList>
+
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" CssClass="text-danger"
+            ErrorMessage=" *Select Borrower is Required" ControlToValidate="ddlBorrower">
+        </asp:RequiredFieldValidator>
+
+        <asp:SqlDataSource ID="SqlDataSource2" runat="server" 
+            ConnectionString="<%$ ConnectionStrings:disk_inventoryjkConnectionString2 %>" 
+            SelectCommand="SELECT [borrower_id], [lname] + ', ' + [fname] as lname 
+            FROM [borrower] ORDER BY [lname]"></asp:SqlDataSource>
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        Select a Checkout Date:</p>
+    <p>
+         <%-- date txtbox validator and compareValidator to current date db --%>
+        <asp:TextBox ID="txtDate" runat="server" TextMode="Date"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" CssClass="text-danger"
+            ErrorMessage=" *Date selection is Required" ControlToValidate="txtDate" Display="Dynamic">
+        </asp:RequiredFieldValidator>
+
+        <asp:CompareValidator ID="CompareValidator1" Operator="LessThanEqual" Type="Date" 
+            ControlToValidate="txtDate" ErrorMessage=" *Please Select Valid Date" Display="Dynamic" 
+            CssClass="text-danger"
+            runat="server" />
+    </p>
+    <p>
+        &nbsp;</p>
+    <p>
+        <%-- Checkout button --%>
+        <asp:Button ID="btnCheckout" runat="server" OnClick="btnCheckout_Click" Text="Checkout" />
+    </p>
+    <p> 
+        &nbsp;</p>
+    <p>
+        <%-- label for final message --%>
+        <asp:Label ID="Label1" runat="server"></asp:Label>
+    </p>
+
+
+
+</asp:Content>
+
+
---
+++ WebApplication1/Maintenance/checkout.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication1.Maintenance
+{
+    public partial class checkout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //validator for date txtbox
+            CompareValidator1.ValueToCompare = DateTime.Now.ToShortDateString();
+        }
+
+        protected void btnCheckout_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                // Declare objects
+                SqlConnection conn;
+                SqlCommand comm;
+
+                // Initialize connection
+                string connectionString = ConfigurationManager.ConnectionStrings["disk_inventoryjkConnectionString2"].ConnectionString;
+                conn = new SqlConnection(connectionString);
+
+                // Create command
+                comm = new SqlCommand(
+                   "EXECUTE sp_ins_checkout @borrower_id, @disk_id, @checkout_date", conn);
+
+                // Add parameter
+                comm.Parameters.Add("@borrower_id", System.Data.SqlDbType.Int);
+                comm.Parameters["@borrower_id"].Value = ddlBorrower.SelectedValue;
+                comm.Parameters.Add("@disk_id", System.Data.SqlDbType.Int);
+                comm.Parameters["@disk_id"].Value = ddlDisk.SelectedValue;
+                comm.Parameters.Add("@checkout_date", System.Data.SqlDbType.Date);
+                comm.Parameters["@checkout_date"].Value = txtDate.Text;
+                // Enclose database code in Try-Catch-Finally
+
+                try
+                {
+                    // Open the connection
+                    conn.Open();
+
+                    // Execute the command
+                    comm.ExecuteNonQuery();
+
+                    // Display completed message
+                    Label1.Text = "*** New Item Checked out: " + ddlBorrower.SelectedItem
+                        + " & Disk:" + ddlDisk.SelectedItem+" ***";
+
+                    //reset ddl and txtbox to blank
+                    ddlDisk.DataBind();
+                    ddlBorrower.Text = "";
+                    txtDate.Text = "";
+
+                }
+                catch (SqlException ex)
+                {
+                    // Display error message
+                    string str;
+                    str = "Source:" + ex.Source;
+                    str += "\n" + "Number:" + ex.Number.ToString();
+                    str += "\n" + "Message:" + ex.Message;
+                    str += "\n" + "Class:" + ex.Class.ToString();
+                    str += "\n" + "Procedure:" + ex.Procedure.ToString();
+                    str += "\n" + "Line Number:" + ex.LineNumber.ToString();
+                    str += "\n" + "Server:" + ex.Server.ToString();
+                    // Display error message
+                    Label1.Text = str;
+                }
+                finally
+                {
+                    // Close the connection
+                    conn.Close();
+                }
+            }
+        }
+
+     
+        //DropDownlist Borrower & Disk Default option text
+        protected void ddlDisk_DataBound(object sender, EventArgs e)
+        {
+            ddlDisk.Items.Insert(0, new ListItem("*** Select Option ***", String.Empty));
+
+        }
+
+        protected void ddlBorrower_DataBound(object sender, EventArgs e)
+        {
+            ddlBorrower.Items.Insert(0, new ListItem("*** Select Option ***", String.Empty));
+
+        }
+    }
+}
